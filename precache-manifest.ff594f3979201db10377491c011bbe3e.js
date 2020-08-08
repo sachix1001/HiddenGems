51fd558e5de7ece78956219458fcd3deb94e9832
sachix1001 <sachix1001@gmail.com>
2020-08-08 22:10:07 +1000

    Updates

---
+++ precache-manifest.ff594f3979201db10377491c011bbe3e.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd8e3090808fced34c549d577201ef9",
+    "url": "/HiddenGems/index.html"
+  },
+  {
+    "revision": "216a64eef114180fe5e1",
+    "url": "/HiddenGems/static/css/4.5a62b327.chunk.css"
+  },
+  {
+    "revision": "1ec0cef0e36126104f6b",
+    "url": "/HiddenGems/static/css/main.3b39b2a4.chunk.css"
+  },
+  {
+    "revision": "64f8fca4c9d9fa2807f2",
+    "url": "/HiddenGems/static/js/0.9ec6caed.chunk.js"
+  },
+  {
+    "revision": "219d60ed32e0cdaa519f",
+    "url": "/HiddenGems/static/js/10.9d808459.chunk.js"
+  },
+  {
+    "revision": "8be415e3337dd8e60af9",
+    "url": "/HiddenGems/static/js/11.ad42fcdf.chunk.js"
+  },
+  {
+    "revision": "35e04c651cf60a46bdd4",
+    "url": "/HiddenGems/static/js/12.50bc893a.chunk.js"
+  },
+  {
+    "revision": "3d46ce8e82112921c975",
+    "url": "/HiddenGems/static/js/13.aa1a05c9.chunk.js"
+  },
+  {
+    "revision": "2d6863e5240da42ba345",
+    "url": "/HiddenGems/static/js/14.32555605.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/HiddenGems/static/js/14.32555605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c064711060810738cf5",
+    "url": "/HiddenGems/static/js/15.09082fd0.chunk.js"
+  },
+  {
+    "revision": "a42388a5d89431dfcd00",
+    "url": "/HiddenGems/static/js/16.f231c337.chunk.js"
+  },
+  {
+    "revision": "92bbfdc84df2a8972738",
+    "url": "/HiddenGems/static/js/17.292c16a5.chunk.js"
+  },
+  {
+    "revision": "e491e0591bb245d4c08e",
+    "url": "/HiddenGems/static/js/18.fb8ed52e.chunk.js"
+  },
+  {
+    "revision": "a28e6242499ef87c064c",
+    "url": "/HiddenGems/static/js/19.6d23a48d.chunk.js"
+  },
+  {
+    "revision": "e9b9186067d9106b28de",
+    "url": "/HiddenGems/static/js/20.654946a5.chunk.js"
+  },
+  {
+    "revision": "75f2a710b7479b44078b",
+    "url": "/HiddenGems/static/js/21.a52d065c.chunk.js"
+  },
+  {
+    "revision": "274f4ea74e052cafda42",
+    "url": "/HiddenGems/static/js/22.3460e627.chunk.js"
+  },
+  {
+    "revision": "5404e2f73a350a09b238",
+    "url": "/HiddenGems/static/js/23.5e941f1b.chunk.js"
+  },
+  {
+    "revision": "ba4cae1a7ca2c62ec869",
+    "url": "/HiddenGems/static/js/24.cea8c4f8.chunk.js"
+  },
+  {
+    "revision": "df6a135cab22d975142e",
+    "url": "/HiddenGems/static/js/25.cb461cff.chunk.js"
+  },
+  {
+    "revision": "4d65d03e55d7161a2d67",
+    "url": "/HiddenGems/static/js/26.b7bf0bdb.chunk.js"
+  },
+  {
+    "revision": "43a51b2b21f4ec156881",
+    "url": "/HiddenGems/static/js/27.6c29d98c.chunk.js"
+  },
+  {
+    "revision": "d0fe9cea75651c21c179",
+    "url": "/HiddenGems/static/js/28.3eede56f.chunk.js"
+  },
+  {
+    "revision": "9d026d6c0240eaf61990",
+    "url": "/HiddenGems/static/js/29.6ad4317c.chunk.js"
+  },
+  {
+    "revision": "0c0f27990f5738533939",
+    "url": "/HiddenGems/static/js/30.b8a99f69.chunk.js"
+  },
+  {
+    "revision": "1d9cf693b1a2bb6cc5dc",
+    "url": "/HiddenGems/static/js/31.8aea8516.chunk.js"
+  },
+  {
+    "revision": "e86aa1965dac35758aaa",
+    "url": "/HiddenGems/static/js/32.9d0fbcef.chunk.js"
+  },
+  {
+    "revision": "9667b4ae20c5bd7afd8d",
+    "url": "/HiddenGems/static/js/33.60d34198.chunk.js"
+  },
+  {
+    "revision": "52fe7684c15068dd74a7",
+    "url": "/HiddenGems/static/js/34.d2d06249.chunk.js"
+  },
+  {
+    "revision": "04a0881d4b4fdb58ac64",
+    "url": "/HiddenGems/static/js/35.482aec49.chunk.js"
+  },
+  {
+    "revision": "d2a4f95f11f040411331",
+    "url": "/HiddenGems/static/js/36.455771b2.chunk.js"
+  },
+  {
+    "revision": "bf9f8c30b2f996453f7f",
+    "url": "/HiddenGems/static/js/37.78fb2187.chunk.js"
+  },
+  {
+    "revision": "9c054960d2e731bd3b5f",
+    "url": "/HiddenGems/static/js/38.5392de20.chunk.js"
+  },
+  {
+    "revision": "aa635f6cb22ffb03fec1",
+    "url": "/HiddenGems/static/js/39.65e14df7.chunk.js"
+  },
+  {
+    "revision": "216a64eef114180fe5e1",
+    "url": "/HiddenGems/static/js/4.3f35fc4d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/HiddenGems/static/js/4.3f35fc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838df30c750b9ab2ccbe",
+    "url": "/HiddenGems/static/js/40.0e256b20.chunk.js"
+  },
+  {
+    "revision": "deb5e4793c71226fbc7f",
+    "url": "/HiddenGems/static/js/41.a58ac06c.chunk.js"
+  },
+  {
+    "revision": "51114bd7c47f522338c7",
+    "url": "/HiddenGems/static/js/42.c5cc4332.chunk.js"
+  },
+  {
+    "revision": "413f29cf0387a59612ef",
+    "url": "/HiddenGems/static/js/43.f7901e99.chunk.js"
+  },
+  {
+    "revision": "1fd98965a3a5b7b13ff9",
+    "url": "/HiddenGems/static/js/44.dff21412.chunk.js"
+  },
+  {
+    "revision": "362d3759d51e296ab3e5",
+    "url": "/HiddenGems/static/js/45.5aa30cf0.chunk.js"
+  },
+  {
+    "revision": "b00186ff274bc8116242",
+    "url": "/HiddenGems/static/js/46.f6ccf532.chunk.js"
+  },
+  {
+    "revision": "ef7d821c1cf6118ec93b",
+    "url": "/HiddenGems/static/js/47.908ee8ed.chunk.js"
+  },
+  {
+    "revision": "97a6985075bd71133e82",
+    "url": "/HiddenGems/static/js/48.dc7dfd00.chunk.js"
+  },
+  {
+    "revision": "d2bfbf2c6790694bd7f0",
+    "url": "/HiddenGems/static/js/49.13def546.chunk.js"
+  },
+  {
+    "revision": "96eb8ef506ab56409885",
+    "url": "/HiddenGems/static/js/5.71bc1f38.chunk.js"
+  },
+  {
+    "revision": "af7599453adadc373244",
+    "url": "/HiddenGems/static/js/50.4b917c79.chunk.js"
+  },
+  {
+    "revision": "76aedb73a7ec4371ba89",
+    "url": "/HiddenGems/static/js/51.93ef48d1.chunk.js"
+  },
+  {
+    "revision": "3fb4a8ea4c639bf79fa1",
+    "url": "/HiddenGems/static/js/52.8af6f594.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/HiddenGems/static/js/52.8af6f594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd577d3ed0b427df2148",
+    "url": "/HiddenGems/static/js/53.75439b3e.chunk.js"
+  },
+  {
+    "revision": "b960652eb230d0bfa076",
+    "url": "/HiddenGems/static/js/54.2683a0c5.chunk.js"
+  },
+  {
+    "revision": "525c8fa6a6bf49477854",
+    "url": "/HiddenGems/static/js/55.343914df.chunk.js"
+  },
+  {
+    "revision": "af0178a74d1d61203017",
+    "url": "/HiddenGems/static/js/56.3946391d.chunk.js"
+  },
+  {
+    "revision": "09dc0b1013bfa5f35fe7",
+    "url": "/HiddenGems/static/js/57.8571d0b6.chunk.js"
+  },
+  {
+    "revision": "8cfceadf069defe3a273",
+    "url": "/HiddenGems/static/js/58.7f6903a9.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/HiddenGems/static/js/58.7f6903a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e70fbe8f22dd27c9290",
+    "url": "/HiddenGems/static/js/6.e3acd6be.chunk.js"
+  },
+  {
+    "revision": "bd56b45d3505fbb329db",
+    "url": "/HiddenGems/static/js/7.01d2c35c.chunk.js"
+  },
+  {
+    "revision": "63c25171e1f9866dffb6",
+    "url": "/HiddenGems/static/js/8.ebe209b9.chunk.js"
+  },
+  {
+    "revision": "a27b7beb6f7f601e224c",
+    "url": "/HiddenGems/static/js/9.a7455924.chunk.js"
+  },
+  {
+    "revision": "1ec0cef0e36126104f6b",
+    "url": "/HiddenGems/static/js/main.0d91a237.chunk.js"
+  },
+  {
+    "revision": "6f606c1fd18fabcf97f8",
+    "url": "/HiddenGems/static/js/polyfills-css-shim.97f0bd4a.chunk.js"
+  },
+  {
+    "revision": "40cd6614211543779b56",
+    "url": "/HiddenGems/static/js/runtime-main.4b3ed030.js"
+  }
+]);
