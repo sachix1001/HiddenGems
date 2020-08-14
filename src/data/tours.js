abe32af01140d3a7187965f32ee63beb19d006dd
sachix1001 <sachix1001@gmail.com>
2020-08-14 12:21:03 +1000

    tour data update with location

--- src/data/tours.js
+++ src/data/tours.js
@@ -34,4 +34,74 @@ export const tours = [
       },
     },
   ],
+  [
+    {
+      name: "SOUTH BANK",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "south-bank",
+      address: "Russell St, South Brisbane QLD 4101",
+      location: {
+        lat: -27.4783104,
+        lng: 153.0148026,
+      },
+    },
+    {
+      name: "Botanic garden",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "botanic-gardens",
+      address: "147 alice st brisbane city qld 4000",
+      location: {
+        lat: -27.4757434,
+        lng: 153.0300051,
+      },
+    },
+    {
+      name: "Lone pine koala sanctuary",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "lone-pine-koala-sanctuary",
+      address: "708 Jesmond Rd, Fig Tree Pocket QLD 4069",
+      location: {
+        lat: -27.5324831,
+        lng: 152.9677457,
+      },
+    },
+  ],
+  [
+    {
+      name: "SOUTH BANK",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "south-bank",
+      address: "Russell St, South Brisbane QLD 4101",
+      location: {
+        lat: -27.4783104,
+        lng: 153.0148026,
+      },
+    },
+    {
+      name: "Botanic garden",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "botanic-gardens",
+      address: "147 alice st brisbane city qld 4000",
+      location: {
+        lat: -27.4757434,
+        lng: 153.0300051,
+      },
+    },
+    {
+      name: "Lone pine koala sanctuary",
+      description:
+        "South Bank is Brisbane’s premier lifestyle and cultural destination. Located on the southern banks of the Brisbane River, its 17 hectares of lush parklands, world-class eateries, stunning river views and hundreds of delightful events all year round make it the perfect place to relax and unwind.",
+      photo: "lone-pine-koala-sanctuary",
+      address: "708 Jesmond Rd, Fig Tree Pocket QLD 4069",
+      location: {
+        lat: -27.5324831,
+        lng: 152.9677457,
+      },
+    },
+  ],
 ];
